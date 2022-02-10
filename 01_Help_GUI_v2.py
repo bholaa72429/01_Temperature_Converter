dc9671197e7939b0cdd5419db6cce8215ba6d403
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2022-02-11 09:50:31 +1300

    Add files via upload
    GUI PT2

---
+++ 01_Help_GUI_v2.py
@@ -0,0 +1,28 @@
+from tkinter import *
+import random
+
+
+class Converter:
+    def __init__(self, parent):
+
+        # Formatting variables..
+        background_color = "light blue"
+
+        # Converter Main Screen GUI..
+        self.converter_frame = Frame(width=300, height=300, bg=background_color)
+        self.converter_frame.grid()
+
+        # Temperature Conversion heading (row 0)
+        self.temp_converter_label = Label(text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter(root)
+    root.mainloop()
