22c311543d2280a79d04fc15d0f0ea14acadf0d2
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2022-02-16 12:13:16 +1300

    Add files via upload

--- 02_converter_GUI_v1.py
+++ 02_converter_GUI_v1.py
@@ -27,7 +27,7 @@ class Converter:
                                             text="Type in the amount to be"
                                                  "converted and then push"
                                                  "one of the buttons below...",
-                                            font="Arial 12 italic", wrap=250,
+                                            font="Arial 10 italic", wrap=250,
                                             justify=LEFT, bg=background_color,
                                             padx=10, pady=10)
         self.temp_instruction_label.grid(row=1)
@@ -37,8 +37,19 @@ class Converter:
                                       font="Arial 14 bold")
         self.to_convert_entry.grid(row=2)
 
-        # Conversion buttons frame (row 3)
+        # Conversion buttons frame (row 3),
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3, pady=10)
 
+        self.to_c_button = Button(self.conversion_buttons_frame,
+                                  text="To Centigrade", font="Arial 10 bold",
+                                  bg="Khaki1", padx=10, pady=10)
+        self.to_c_button.grid(row=0, column=0)
+
+        self.to_f_button = Button(self.conversion_buttons_frame,
+                                  text="To Fahrenheit", font="Arial 10 bold",
+                                  bg="Orchid1", padx=10, pady=10)
+        self.to_f_button.grid(row=0, column=1)
         # Answer label (row 3)
 
         # History / Help button frame (row 5)
