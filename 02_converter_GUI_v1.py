53169b80a9bdaf8c653c0a35aeb7e9fd18b66290
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2022-02-16 11:55:32 +1300

    Add files via upload

--- 02_converter_GUI_v1.py
+++ 02_converter_GUI_v1.py
@@ -5,7 +5,43 @@ import random
 
 class Converter:
     def __init__(self, parent):
-        print("hello world")
+
+        # formatting variable
+        background_color = "light blue"
+
+        # Converter Frame
+        self.converter_frame = Frame(width=300, bg=background_color,
+                                     pady=10)
+        self.converter_frame.grid()
+
+        # Temperature Converter heading (row 0)
+        self.temp_heading_label = Label(self.converter_frame,
+                                        text="Temperature Coverter",
+                                            font="Arial 16 bold",
+                                            bg=background_color,
+                                            padx=10, pady=10)
+        self.temp_heading_label.grid(row=0)
+
+        # User instructions (row 1)
+        self.temp_instruction_label = Label(self.converter_frame,
+                                            text="Type in the amount to be"
+                                                 "converted and then push"
+                                                 "one of the buttons below...",
+                                            font="Arial 12 italic", wrap=250,
+                                            justify=LEFT, bg=background_color,
+                                            padx=10, pady=10)
+        self.temp_instruction_label.grid(row=1)
+
+        # Temperature entry box (row 2)
+        self.to_convert_entry = Entry(self.converter_frame, width=20,
+                                      font="Arial 14 bold")
+        self.to_convert_entry.grid(row=2)
+
+        # Conversion buttons frame (row 3)
+
+        # Answer label (row 3)
+
+        # History / Help button frame (row 5)
 
 
 # main routine
