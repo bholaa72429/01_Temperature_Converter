0b510ff8ab901d45ce93ec589f50fc4b529cd356
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2022-03-04 09:48:45 +1300

    Add files via upload

--- 10_History_GUI_v1.py
+++ 10_History_GUI_v1.py
@@ -44,22 +44,61 @@ class Converter:
         get_history = history(self)
         get_history.history_text.configure(text="History text goes here")
 
-class history:
-    def __init__(self, parent):
 
-        backgrond = "#a9ef99"  # Pale green
+if __name__ == '__main__':
+    class history:
+        def __init__(self, partner):
 
-        # disable history button
-        partner.history_button.config(state=DISABLED)
+            backgrond = "#a9ef99"  # Pale green
 
+            # disable history button
+            partner.history_button.config(state=DISABLED)
 
+            # sets up child window (ie. history box)
+            self.history_box = Toplevel()
 
-    def round_it(self, to_round):
-        if to_round % 1 == 0:
-            rounded = int(to_round)
-        else:
-            rounded = round(to_round, 1)
-        return rounded
+            #  If users press cross at top, close history and 'release' history
+            self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
+
+            # Set up GUI Frame
+            self.history_frame = Frame(self.history_box, width=300, bg=backgrond)
+            self.history_frame.grid()
+
+            # set up history heading (row 0)
+            self.how_heading = Label(self.history_frame, text="Calculation History",
+                                     font="arial 19 bold", bg=backgrond)
+            self.how_heading.grid(row=0)
+
+            # history text (label, row 1 )
+            self.history_text = Label(self.history_frame,
+                                      text="Here are your most recent"
+                                      "calculation. Please use the "
+                                      "export button to create a text"
+                                      "file of all your calculation for"
+                                      "this session", wrap=250, font="arial 10 italic",
+                                      justify=LEFT, width=40, bg=backgrond)
+            self.history_text.grid(row=1)
+
+            # History Output goes here (row 2)
+
+            # Export / Dismiss Button Frame (row 3)
+            self.export_dismiss_frame = Frame(self.history_frame)
+            self.export_dismiss_frame.grid(row=3, pady=10)
+
+            # Export Button
+            self.export_button = Button(self.export_dismiss_frame, text="Export",
+                                        font="Arial 12 bold")
+            self.export_button.grid(row=0, column=0)
+
+            # Dismiss Button
+            self.dismiss_button = Button(self.export_dismiss_frame, text="Dismiss",
+                                         font="Arial 12 bold", command=partial(self.close_history, partner))
+            self.dismiss_button.grid(row=0, column=1)
+
+        def close_history(self, partner):
+            # Put history button back to normal...
+            partner.history_button.config(state=NORMAL)
+            self.history_box.destroy()
 
 # main routine
 if __name__ == "__main__":
