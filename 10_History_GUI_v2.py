ed9b9525b886b47ca1093f11845c23f7229e2447
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2022-03-07 14:48:19 +1300

    Add files via upload

--- 10_History_GUI_v2.py
+++ 10_History_GUI_v2.py
@@ -41,15 +41,17 @@ class Converter:
         #if len(self.all_calc_list) == 0:
             #self.history_button.config(state=DISABLED)
 
-
     def history(self, calc_history):
         History(self, calc_history)
 
 
 class History:
+
     def __init__(self, partner, calc_history):
 
-        backgrond = "#a9ef99"  # Pale green
+        print(calc_history)
+
+        background = "#a9ef99"  # Pale green
 
         # disable history button
         partner.history_button.config(state=DISABLED)
@@ -61,22 +63,22 @@ class History:
         self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
 
         # Set up GUI Frame
-        self.history_frame = Frame(self.history_box, width=300, bg=backgrond)
+        self.history_frame = Frame(self.history_box, width=300, bg=background)
         self.history_frame.grid()
 
         # set up history heading (row 0)
         self.how_heading = Label(self.history_frame, text="Calculation History",
-                                 font="arial 19 bold", bg=backgrond)
+                                 font="arial 19 bold", bg=background)
         self.how_heading.grid(row=0)
 
         # history text (label, row 1 )
         self.history_text = Label(self.history_frame,
                                   text="Here are your most recent"
-                                  "calculation. Please use the "
+                                  "calculations. Please use the "
                                   "export button to create a text"
                                   "file of all your calculation for"
                                   "this session", wrap=250, font="arial 10 italic",
-                                  justify=LEFT, width=40, bg=backgrond)
+                                  justify=LEFT, width=40, bg=background)
         self.history_text.grid(row=1)
 
         # History Output goes here (row 2)
@@ -88,18 +90,18 @@ class History:
             for item in range(0,7):
                 history_string += calc_history[len(calc_history)-item -1]+"\n"
 
-            else:
-                for item in calc_history:
-                    history_string += calc_history[len(calc_history) -
-                                                 calc_history.index(item) - 1] + "\n"
-                    self.history_text.config(text="here is your calculation "
-                                                  "history. You can use the "
-                                                  "export button to save this "
-                                                  "data to text file if desired!!")
+        else:
+            for item in calc_history:
+                history_string += calc_history[len(calc_history) -
+                                             calc_history.index(item) - 1] + "\n"
+                self.history_text.config(text="here is your calculation "
+                                              "history. You can use the "
+                                              "export button to save this "
+                                              "data to text file if desired!!")
 
         # Label to display calculation history to user
         self.calc_label = Label(self.history_frame, text=history_string,
-                                bg=backgrond,font="Arial 12", justify=LEFT)
+                                bg=background,font="Arial 12", justify=LEFT)
 
         self.calc_label.grid(row=2)
 
